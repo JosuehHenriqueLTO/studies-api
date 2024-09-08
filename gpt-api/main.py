d11c4d2d77e44d857f81e10e94e35daaa34f712d
dev_JosuehHenrique <josuehenrique2004@gmail.com>
2024-09-07 23:00:35 -0300

    Update main.py
    Quick changes to a better optmization and tests.

--- gpt-api/main.py
+++ gpt-api/main.py
@@ -1,17 +1,19 @@
-import openai
 from openai import OpenAI
+
 from key import API_KEY
 
 client = OpenAI(api_key=API_KEY)
 
+
 def talking(message):
-  completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-      {"role": "user", "content": message}
-    ]
-  )
-  return completion.choices[0].message
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "assistant", "content": "Olá, fico muito contente em ajudar em qualquer que seja o assunto!"},
+            {"role": "user", "content": message}
+        ]
+    )
+    return completion.choices[0].message
+
 
 print(talking("Tempo tá bom aí?"))
-
